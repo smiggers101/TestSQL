399fa896b811095ad5f963f252b426b85589b321
GrahamS <smiggers@gmail.com>
2018-03-22 11:15:29 +0000

    tidy

--- file1.sql
+++ file1.sql
@@ -1,3 +1,6 @@
 select EMPLID from PS_JOB where EMPLID = '1234'
 
-select J.EMPLID from PS_JOB J where EMPLID = '1234' and EFFSEQ = (select max(EFFSEQ) from PS_JOB J2 WHERE J.EMPLID = J2.EMPLID AND J2.EFFSEQ <= GETDATE())
+select J.EMPLID from PS_JOB J 
+
+where   EMPLID = '1234' and 
+        EFFSEQ = (select max(EFFSEQ) from PS_JOB J2 WHERE J.EMPLID = J2.EMPLID AND J2.EFFSEQ <= GETDATE())
